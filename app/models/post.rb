3b9b220cb4de7663c1087cc515a8036083e60b83
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-01-08 13:20:09 +0100

    add data processing model

---
+++ app/models/post.rb
@@ -0,0 +1,18 @@
+class Post < ApplicationRecord
+    belongs_to :user
+    has_many :likes
+    has_many :comments
+
+    after_save :update_post_counter
+
+    def five_recent_comments
+        comments.limit(5).order(created_at: :desc)
+    end
+
+    private
+
+    def update_post_counter
+        user.increment!(:post_counter)
+    end
+
+end
