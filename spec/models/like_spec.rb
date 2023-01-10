eaea8eb8e0cca6833740ca4d879c18cea4bc5e56
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-01-10 17:14:33 +0100

    add all final tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   context 'Testing the like model' do
-    # subject do
-
-    # end
 
     it 'is valid with valid attributes' do
       @user01 = User.create(name: 'pires', photo: 'https://image.com/01.jpg', bio: 'Born in USA', posts_counter: 0)
@@ -15,9 +12,9 @@ RSpec.describe Like, type: :model do
 
     it 'update_likes_counter' do
       @user01 = User.create(name: 'pires', photo: 'https://image.com/01.jpg', bio: 'Born in USA', posts_counter: 0)
-      @post01 = Post.create(user_id: @user01.id, title: 'Hello Mark', text: 'What about you ?', likes_counter: 1,
+      @post01 = Post.create(user: @user01, title: 'Hello Mark', text: 'What about you ?', likes_counter: 1,
                             comments_counter: 1)
-      Like.create(user_id: @user01.id, post_id: @post01.id)
+      Like.create(user: @user01, post_id: @post01)
       expect(@post01.likes_counter).to eq 1
     end
   end
