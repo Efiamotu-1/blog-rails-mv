e60633043f0b840ee54e01cefe2beb2a86bfb7ce
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-01-17 17:57:24 +0100

    add integration specs and fix n+1 problems

---
+++ spec/integration/post_index_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Post Index Test (Assert)', type: :feature do
+  before(:each) do
+    @user = User.create(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Physics Teacher',
+                        posts_counter: 0)
+    @post = Post.create(user: @user, title: 'Welcome', text: 'this is the first test post', likes_counter: 0,
+                        comments_counter: 0)
+    # @post2 = Post.create(user: @user, title: 'Welcome home', text: 'this is the second test post', likes_counter: 0, comments_counter: 0)
+    # @post3 = Post.create(user: @user, title: 'Welcome here', text: 'this is the third test post', likes_counter: 0, comments_counter: 0)
+    
+    5.times { Comment.create(text: 'Nice post!!', user_id: @user.id, post_id: @post.id) }
+    visit user_posts_path(@user.id)
+  end
+
+  describe 'Post Index' do
+    scenario "shows the user's username" do
+      expect(page).to have_content(@user.name)
+    end
+
+    scenario 'displays post text' do
+      expect(page).to have_content('this is the first test post')
+    end
+
+    scenario 'display the post title' do
+      expect(page).to have_content(@post.title)
+    end
+
+    scenario 'display the post body' do
+      expect(page).to have_content(@post.text)
+    end
+  
+    scenario 'display the first comment on a post' do
+      expect(page).to have_content('Nice post!!')
+    end
+  
+    scenario 'display the how many comments' do
+      expect(page).to have_content('Comments: 5')
+    end
+  
+    scenario 'display the how many Likes' do
+      expect(page).to have_content('Likes: 0')
+    end
+  end
+end
---
+++ spec/integration/user_index_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'User Index Asserts', type: :system do
+  before :each do
+    @user1 = User.create(name: 'Chris stapleton', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                         bio: 'I am a Country musician', posts_counter: 6)
+    @user2 = User.create(name: 'Mylie cyrus', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                         bio: 'I am a pop artiste', posts_counter: 9)
+  end
+
+  describe 'index page' do
+    it 'shows the right content' do
+      visit root_path
+      expect(page).to have_content('Users')
+    end
+  end
+
+  scenario 'shows all created users' do
+    visit users_path
+    expect(page).to have_content(@user1.name)
+    expect(page).to have_content(@user2.name)
+  end
+
+  scenario 'renders the user post count' do
+    visit users_path
+    expect(page).to have_content("Number of posts: #{@user2.posts_counter}")
+  end
+end
+
+RSpec.describe 'User Index Redirects', type: :system do
+  describe 'Index Page' do
+    before :each do
+      @current_user = User.create(name: 'Habeeb Efiamotu', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                                  bio: 'I am a software developer', posts_counter: 2)
+    end
+
+    scenario 'renders the user' do
+      visit users_path
+      click_link(@current_user.name)
+      sleep(1)
+      expect(current_path).to match user_path(@current_user)
+    end
+  end
+end
