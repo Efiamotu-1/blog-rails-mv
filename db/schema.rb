f7ef4737e8016235fe3f254edfdaff0c988bb736
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-01-27 00:43:11 +0100

    fix validation erros

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_07_171216) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "text"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -34,10 +34,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_07_171216) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text "title"
-    t.string "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_07_171216) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
