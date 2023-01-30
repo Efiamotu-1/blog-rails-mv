e0355d280d1d8a3530eb96db731cb64552155951
Josué Benavides <jdbs9514@gmail.com>
2023-01-30 17:34:13 -0500

    fix the n+1 problem and update the readme file

--- README.md
+++ README.md
@@ -118,7 +118,11 @@ To run the project, execute the following command:
 | 👤 Name | Github | LinkedIn |
 |------|--------|----------|
 |Habeeb Efiamotu Musa Owolewa|[@Efiamotu-1](https://github.com/Efiamotu-1)|[Habeeb Efiamotu Musa](https://www.linkedin.com/in/musa-habeeb/)|
-<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+| 👤 Name | Github | LinkedIn |
+|------|--------|----------|
+|Josué David Benavides Salinas|[@jdbs9514](https://github.com/jdbs9514)|[Josué David Benavides Salinas](https://www.linkedin.com/in/macoin/)|
+
 
 
 ## 🔭 Future Features <a name="future-features"></a>
--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Post Index Test (Assert)', type: :feature do
                         comments_counter: 0)
     # @post2 = Post.create(user: @user, title: 'Welcome home', text: 'this is the second test post', likes_counter: 0, comments_counter: 0)
     # @post3 = Post.create(user: @user, title: 'Welcome here', text: 'this is the third test post', likes_counter: 0, comments_counter: 0)
-    
+
     5.times { Comment.create(text: 'Nice post!!', user_id: @user.id, post_id: @post.id) }
     visit user_posts_path(@user.id)
   end
@@ -29,15 +29,15 @@ RSpec.describe 'Post Index Test (Assert)', type: :feature do
     scenario 'display the post body' do
       expect(page).to have_content(@post.text)
     end
-  
+
     scenario 'display the first comment on a post' do
       expect(page).to have_content('Nice post!!')
     end
-  
+
     scenario 'display the how many comments' do
       expect(page).to have_content('Comments: 5')
     end
-  
+
     scenario 'display the how many Likes' do
       expect(page).to have_content('Likes: 0')
     end
